8168cdcbdb39c09e3c3d885595be3fb4074e6998
DJ-27 <dj.270319200420@gmail.com>
2024-04-29 13:25:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main() =>runApp(MaterialApp(
+home: Scaffold(
+  appBar:AppBar(
+    title: Center(
+      child: Text('awtwreyweryq'),
+    ),
+    backgroundColor: Colors.blueAccent,
+  ),
+  backgroundColor: Colors.red[200],
+  floatingActionButton:
+),
+));
+
+
